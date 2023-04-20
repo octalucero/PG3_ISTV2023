a8ceea5acfaca8dc7cd4708bdc7fe875bc483188
octalucero <octaviolucero2005@gmail.com>
2023-04-20 18:23:04 -0300

    guia3 ej2

---
+++ Ejercicios/Guia 3/Guia3_ej2.py
@@ -0,0 +1,18 @@
+def numero():
+    try:
+        num = int(input("Ingrese un numero: "))
+        return num
+    except ValueError:
+        print("Ingreso de numero erroneo, pruebe otra vez")
+        return numero()
+    
+def division(dividendo, divisor):
+    try:
+        divid = dividendo / divisor
+        return divid
+    except ZeroDivisionError:
+        print("No es posible didivir por 0 ")
+
+num1 = numero()
+num2 = numero()
+print(division(num1, num2))
